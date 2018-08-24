e9fcc1061e4fb171b7de26478a1e727e39b8e561
Patrik S <absolut.patrik@hotmail.com>
2018-08-24 10:06:15 +0200

    working bouncyballs

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    public List<Transform> positions;
+    public List<GameObject> balls;
+
+    List<Transform> pathBall1 = new List<Transform>(); 
+    List<Transform> pathBall2 = new List<Transform>();
+
+    int originBall1;
+    int originBall2;
+
+    public float moveDelay = 1f;
+
+    private void Start()
+    { 
+
+        //starting positions
+        balls[0].transform.position = positions[1].position;
+        balls[1].transform.position = positions[4].position;
+        originBall1 = 1;
+        originBall2 = 4;
+
+        pathBall1 = RandomizePath(0, originBall1);
+        pathBall2 = RandomizePath(1, originBall2);
+
+        StartCoroutine(FollowPath(0, pathBall1));
+        StartCoroutine(FollowPath(1, pathBall2));
+
+    }
+
+    private void Update()
+    { 
+
+
+    }
+
+
+
+    IEnumerator FollowPath(int ball, List<Transform> pathpositions)
+    {
+
+        for (int i = 0; i < pathpositions.Count; i++)
+        {
+            balls[ball].transform.position = pathpositions[i].position;
+            //Debug.Log("Moving " + ball);
+            yield return new WaitForSeconds(moveDelay);
+        }
+
+        if (ball == 0)
+        {
+            pathBall1 = RandomizePath(ball, originBall1);
+            StartCoroutine(FollowPath(ball, pathBall1));
+        }
+        else
+        {
+            pathBall2 = RandomizePath(ball, originBall2);
+            StartCoroutine(FollowPath(ball, pathBall2));
+        }
+    }
+
+
+    List<Transform> RandomizePath(int ball, int origin)
+    {
+        List<Transform> path = new List<Transform>();
+
+        if (origin < 3)
+            path = MakePath(ball, origin);
+        else if (origin >= 3)
+        {
+            path = MakeReversePath(ball, origin);
+        }
+
+        return path;
+    }
+
+    List<Transform> MakePath(int ball, int origin)
+    {
+        List<Transform> path = new List<Transform>();
+        int rn = Random.Range(0,3);
+
+        for (int i = 0; i < 6; i++)
+        {
+            path.Add(positions[origin].GetChild(rn).GetChild(i).transform);
+        }
+
+        Path endpos = positions[origin].GetChild(rn).GetComponent<Path>();
+
+        if (ball == 0)
+        {
+            originBall1 = endpos.end;
+        }
+        else
+        {
+            originBall2 = endpos.end;
+        }
+
+        return path;
+    }
+    
+    List<Transform> MakeReversePath(int ball, int origin)
+    {
+        List<Transform> path = new List<Transform>();
+        int rn = Random.Range(0, 3);
+        path.Add(positions[rn].transform);
+
+        Path endpos = positions[0].GetChild(0).GetComponent<Path>();
+
+        switch (origin)
+        {
+            case 3:
+                for (int i = 0; i < 6; i++)
+                {
+                    path.Add(positions[rn].GetChild(0).GetChild(i).transform);
+                }
+                endpos = positions[rn].GetChild(0).GetComponent<Path>();
+
+                break;
+            case 4:
+                for (int i = 0; i < 6; i++)
+                {
+                    path.Add(positions[rn].GetChild(1).GetChild(i).transform);
+                }
+                endpos = positions[rn].GetChild(0).GetComponent<Path>();
+                break;
+            case 5:
+                for (int i = 0; i < 6; i++)
+                {
+                    path.Add(positions[rn].GetChild(2).GetChild(i).transform);
+                }
+                endpos = positions[rn].GetChild(0).GetComponent<Path>();
+                break;
+
+        }
+
+        if (ball == 0)
+        {
+            originBall1 = endpos.start;
+        } else
+        {
+            originBall2 = endpos.start;
+        }
+
+
+        path.Reverse();
+        return path;
+    }
+}
---
+++ Assets/Scripts/JugglerManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JugglerManager : MonoBehaviour {
+
+    public List<GameObject> jugglers = new List<GameObject>();
+    public List<Transform> positions;
+
+    GameObject left;
+    GameObject mid;
+    GameObject right;
+    int position = 1;
+
+    private void OnEnable()
+    {
+        Input.OnLeftPressed += Input_OnLeftPressed;
+        Input.OnRightPressed += Input_OnRightPressed;
+
+        left = jugglers[0].gameObject;
+        mid = jugglers[1].gameObject;
+        right = jugglers[2].gameObject;
+
+        left.SetActive(false);
+        mid.SetActive(true);
+        right.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        Input.OnLeftPressed -= Input_OnLeftPressed;
+        Input.OnRightPressed -= Input_OnRightPressed;
+    }
+
+
+    private void Input_OnRightPressed()
+    {
+        //Debug.Log("Right pressed");
+        if (StanceDance(1))
+            position++;
+    }
+
+    private void Input_OnLeftPressed()
+    {
+        //Debug.Log("Left pressed");
+        if (StanceDance(-1))
+            position--;
+    }
+
+
+
+    bool StanceDance(int direction)
+    {
+        switch(direction)
+        {
+            case -1:
+                if (position == 2)
+                {
+                    right.SetActive(false);
+                    mid.SetActive(true);
+                }
+                else if (position == 1)
+                {
+                    mid.SetActive(false);
+                    left.SetActive(true);
+                }
+                else
+                    return false;
+                break;
+            case 1:
+                if (position == 0)
+                {
+                    left.SetActive(false);
+                    mid.SetActive(true);
+                }
+                else if (position == 1)
+                {
+                    mid.SetActive(false);
+                    right.SetActive(true);
+                }
+                else
+                    return false;
+                break;
+
+        }
+        return true;
+    }
+}
